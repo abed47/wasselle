f63a7327dac7792797352783d90eb17b91e2e6f3
abdull-rahman <abed472011@gmail.com>
2020-03-27 05:59:27 +0200

    added loading spinner to categories page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect, useContext} from 'react';
 import {CategoryListItem} from './CategoryListItem';
 import {MainContext} from './../context';
 import {firebase} from './../firebase';
+import {ClimbingBoxLoader} from 'react-spinners';
 export const Categories = () => {
     const db = firebase.firestore();
     const contextValues = useContext(MainContext);
@@ -31,8 +32,18 @@ export const Categories = () => {
     return(
         <div className="categories">
             {
-                categories.length == 0 ?
-                 (<div>Loading</div>) :
+                categories.length === 0 ?
+                 (
+                    <div className="sweet-loading">
+                    <ClimbingBoxLoader
+                      css={`justify-self: center`}
+                      size={150}
+                      color={"#5CC062"}
+                      loading={true}
+                      size={18}
+                    />
+                  </div>
+                 ) :
                 categories.map(category => {
                     return(<CategoryListItem key={category.id} iconName={category.iconName} title={category.name} id={category.id} />)
                 })
