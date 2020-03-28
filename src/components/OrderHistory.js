1cffa2aad6c3735cbf175a612a8ec5f0c3e0a10d
abdull-rahman <abed472011@gmail.com>
2020-03-29 01:35:09 +0300

    added date function and get orders from db

--- src/components/OrderHistory.js
+++ src/components/OrderHistory.js
@@ -1,30 +1,120 @@
-import React, { useState } from 'react';
-import {FaRegClock, AiOutlineFileDone} from 'react-icons/fa'
-import {FaExclamationTriangle} from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import {FaRegClock, FaExclamationTriangle} from 'react-icons/fa'
+import { AiOutlineFileDone} from 'react-icons/ai'
 import {AiOutlineAlert} from 'react-icons/ai'
 import {firebase} from './../firebase';
 import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
+import {MainContext} from './../context';
 import {ClockLoader} from 'react-spinners'
 export const OrderHistory = ({match}) => {
     const [showLoading, setShowLoading] = useState(true)
+    const [orderList, setOderList] = useState([]);
+    const [noItems, setNoItems] = useState(false)
     let fs = firebase.firestore();
+    let ordersRef = fs.collection('orders');
+    
+    async function getOrders(){
+        console.log(match.params.id)
+        let placeHolderArr = orderList;
+        await ordersRef.where('uid', '==', `${match.params.id}`)
+        .get()
+        .then(docs => {
+            if(docs.empty){
+                setShowLoading(false)
+                setNoItems(true)
+                return
+            }
+
+            docs.forEach(doc => {
+                let obj = doc.data();
+                obj.id = doc.id;
+                placeHolderArr.push(obj);
+            })
+        }).then(() => {
+            setOderList(placeHolderArr)
+            if(placeHolderArr.length){
+                return setShowLoading(false);
+            }
+            setNoItems(true)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    function getIcon(iconName){
+        switch (iconName){
+            case 'pending':
+                return <FaRegClock />
+                break;
+            case 'done':
+                return <AiOutlineFileDone/>
+                break;
+        }
+    }
+    
+    function getDate(date){
+        let d = new Date(date);
+        let now = new Date().getTime();
+        let def = now - date;
+        if(def < 60000){
+            return `${Math.floor(def/1000)} sec`
+        }
+
+        if(def < 3600000){
+            let m = Math.floor(def/60000)
+            return `${m} min`
+        }
+
+        if(def < 86400000){
+            let h = Math.floor(def / 3600000)
+            return `${h} h`
+        }
+
+        if(def > 86400 && def < 604800000){
+            let days = Math.floor(def / 86400000);
+            return `${days} days`
+        }
+
+        return `${d.getDate()} / ${d.getMonth() + 1}`;
+        
+    }
+    useEffect(() => {
+        getOrders()
+    }, [])
+
     return(
-       <div>
+       <div className="orderhistory">
            <Modal
-            style={{
-                content:{
-                    top: '50%',
-                    left: '50%',
-                    right: 'auto',
-                    bottom: 'auto',
-                    marginRight: '-50%',
-                    transform: 'translate(-50%, -50%)'
-                }
-            }}
-            isOpen={showLoading}>
-            <ClockLoader size={'60px'} color={'#5cc062'}/>
+           ariaHideApp={false}
+                style={{
+                    content:{
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)'
+                    }
+                }}
+                isOpen={showLoading}>
+                <ClockLoader size={'60px'} color={'#5cc062'}/>
            </Modal>
+
+           {
+               !noItems ? null : (<span className="empty__page">no Itmes</span>)
+           }
+
+           {
+               !orderList.length ? null :
+               orderList.map(item => {
+               return <Link to={`/orderView/${item.id}`} key={item.id} className="orderhistory__item">
+                   <span>{item.id.substring(0,5)} ...</span>
+                   <span>{getDate(item.date)}</span>
+               <span>{item.status}{getIcon(item.status)}</span>
+               </Link>
+               })
+           }
        </div>
     )
 }
