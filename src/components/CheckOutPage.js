bbd47602a132275c630712a411872731716eed6c
abdull-rahman <abed472011@gmail.com>
2020-03-28 04:06:59 +0200

    styled checkout

--- src/components/CheckOutPage.js
+++ src/components/CheckOutPage.js
@@ -1,4 +1,4 @@
-import React,{useContext,useEffect} from 'react';
+import React,{useContext,useEffect, useState} from 'react';
 import {MainContext} from './../context';
 
 export const CheckOutPage = () => {
@@ -6,49 +6,83 @@ export const CheckOutPage = () => {
     const {setPage} = contextValues.page;
     const {UserObject} = contextValues.user
     const {cart,setCart} = contextValues.cart
-    
+    const [user, setUser] = useState({});
     function checkout(){
 
     }
 
+    function getCartTotal(arr){
+        let total  = 0;
+        if(arr.length < 1){
+            return total;
+        }
+
+        arr.forEach(item => {
+            total = total + (item.orderedQuantity * item.unitPrice)
+        })
+        
+        return total;
+    }
+
     useEffect(() => {
+        if(UserObject){
+            setUser(JSON.parse(localStorage.getItem('user')))
+        }
         setPage('checkout')
-        console.log(cart)
     },[]);
 
     return(
         <div className="checkout">   
             <div className="checkout__container">
                 <div className="checkout__items">
-                    <div className="checkout__item">
-                        <span className="item__name">item 1</span>
-                        <span className="item__quantity">x 7</span>
-                        <span className="item__price">1254 L.L</span>
-                    </div>
+                    
+                    {
+                        cart.map(item => {
+                            return (
+                                <div className="checkout__item">
+                                    <span className="item__name">{item.name}</span>
+                                    <span className="item__quantity">x {item.orderedQuantity}</span>
+                                    <span className="item__price">{item.orderedQuantity * item.unitPrice} L.L</span>
+                                </div>
+                            )
+                        })
+                    }
 
-                    <div className="checkout__item">
-                        <span className="item__name">item 1</span>
-                        <span className="item__quantity">x 7</span>
-                        <span className="item__price">1254 L.L</span>
-                    </div>
-
-                    <div className="checkout__item">
-                        <span className="item__name">item 1</span>
-                        <span className="item__quantity">x 7</span>
-                        <span className="item__price">1254 L.L</span>
-                    </div>
-
-                    <div className="checkout__item">
-                        <span className="item__name">item 1</span>
-                        <span className="item__quantity">x 7</span>
-                        <span className="item__price">1254 L.L</span>
-                    </div>
                 </div>
-                <span className="checkout__user">abed</span>
-                <span className="checkout__phone">phone</span>
-                <span className="checkout__location">location</span>
-                <span className="checkout__total">12551 L.L</span>
-                <button className="button__confirm">Cofirm</button>
+                <span className="checkout__user">
+                    {
+                        user.displayName ? user.displayName :
+                        <span className="checkout__input__group">
+                            <label className="d-block">name:</label>
+                            <input />
+                        </span>
+                    }
+                </span>
+                <span className="checkout__phone">
+                    {
+                        user.phoneNumber ? user.phoneNumber :
+                        <span className="checkout__input__group">
+                            <label className="d-block">phone:</label>
+                            <input />
+                        </span>
+                    }
+                </span>
+                <span className="checkout__location">
+                    {
+                        user.location ? user.location :
+                        <button className="button__primary__outline">Get Location</button>
+                    }
+                </span>
+                <span className="checkout__input__group">
+                    <label className="d-block">building:</label>
+                    <input />
+                </span>
+                <span className="checkout__input__group">
+                    <label className="d-block">floor:</label>
+                    <input />
+                </span>
+                <span className="checkout__total d-block"> {getCartTotal(cart)} L.L</span>
+                <button className="button__confirm d-block">Cofirm</button>
             </div>
         </div>
     )
