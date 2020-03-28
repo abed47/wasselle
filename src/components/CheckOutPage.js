efbc5c39aacb4c6409943135b5ed8453f268ee7f
abdull-rahman <abed472011@gmail.com>
2020-03-28 02:00:20 +0200

    added checkout layout

--- src/components/CheckOutPage.js
+++ src/components/CheckOutPage.js
@@ -13,12 +13,43 @@ export const CheckOutPage = () => {
 
     useEffect(() => {
         setPage('checkout')
-        console.log(UserObject)
+        console.log(cart)
     },[]);
 
     return(
-        <div className="checkout__container">
-            
+        <div className="checkout">   
+            <div className="checkout__container">
+                <div className="checkout__items">
+                    <div className="checkout__item">
+                        <span className="item__name">item 1</span>
+                        <span className="item__quantity">x 7</span>
+                        <span className="item__price">1254 L.L</span>
+                    </div>
+
+                    <div className="checkout__item">
+                        <span className="item__name">item 1</span>
+                        <span className="item__quantity">x 7</span>
+                        <span className="item__price">1254 L.L</span>
+                    </div>
+
+                    <div className="checkout__item">
+                        <span className="item__name">item 1</span>
+                        <span className="item__quantity">x 7</span>
+                        <span className="item__price">1254 L.L</span>
+                    </div>
+
+                    <div className="checkout__item">
+                        <span className="item__name">item 1</span>
+                        <span className="item__quantity">x 7</span>
+                        <span className="item__price">1254 L.L</span>
+                    </div>
+                </div>
+                <span className="checkout__user">abed</span>
+                <span className="checkout__phone">phone</span>
+                <span className="checkout__location">location</span>
+                <span className="checkout__total">12551 L.L</span>
+                <button className="button__confirm">Cofirm</button>
+            </div>
         </div>
     )
 }
