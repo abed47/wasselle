ffca625afc609b614e53e43e46f95f8a90f7b047
abdull-rahman <abed472011@gmail.com>
2020-04-09 02:51:47 +0300

    created location component, installed react-google-maps

---
+++ src/components/LocationComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LocationComponent = () => {
+
+    return (
+        <div className="container">
+            
+        </div>
+    )
+}
+
+export default LocationComponent;
